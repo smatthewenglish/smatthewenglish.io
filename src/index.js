77654daef82fd88c676da507d9c2465e8a7335db
smatthewenglish <s.matthew.english@gmail.com>
2020-03-17 20:16:05 -0400

    ~

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { render } from "react-dom";
+import ParticlesBg from "particles-bg";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <ParticlesBg type="lines" num={1000} bg={true}/>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
