012f6f140bb5f1560f49f49b286dec797ced1a48
smatthewenglish <s.matthew.english@gmail.com>
2020-03-24 13:39:16 -0400

    x

--- src/MenuTsc.js
+++ src/MenuTsc.js
@@ -3,45 +3,43 @@ import React from 'react';
  
 const MenuTsc = () => {
     return (
-     
-    <div class="lolol">
+<div class="box" id="hook">
 
-<div id="background-wrap">
+    <div class="row header">
+        <div class="lolol">        
+            <div id="background-wrap">
+                <div class="x1"><div class="cloud"></div></div>
+                <div class="x2"><div class="cloud"></div></div>
+                <div class="x3"><div class="cloud"></div></div>
+                <div class="x4"><div class="cloud"></div></div>
+                <div class="x5"><div class="cloud"></div></div>
 
-    <div class="x1">
-        <div class="cloud"></div>
+                <img class="craftsmanship" src="./craftsmanship.gif"/>
+            </div>
+        </div>
+    </div>        
+    <div class="row content">
+        <div id="root"></div> 
     </div>
-
-    <div class="x2">
-        <div class="cloud"></div>
-    </div>
-
-    <div class="x3">
-        <div class="cloud"></div>
-    </div>
-
-    <div class="x4">
-        <div class="cloud"></div>
-    </div>
-
-    <div class="x5">
-        <div class="cloud"></div>
-    </div>
-<img class="craftsmanship" src="./craftsmanship.gif"/>
-
+    <div class="row footer">
+        <div class="footer-social-icons">
+          <a class="social-icon" href="//www.meetup.com/members/216718973/"><i class="fa fa-meetup"></i></a>
+          <a class="social-icon" href="//t.me/smatthewenglish"><i class="fa fa-telegram"></i></a>
+          <a class="social-icon" href="//stackoverflow.com/users/3787253/smatthewenglish"><i class="fa fa-stack-overflow"></i></a>
+          <a class="social-icon" href="//apps.apple.com/us/developer/s-matthew-english/id1458676174"><i class="fa fa-apple"></i></a> 
+          <a class="social-icon" href="mailto:matthew@tschess.io"><i class="fa fa-envelope"></i></a>
+          <a class="social-icon" href="//scholar.google.de/citations?user=Wnka3RQAAAAJ&hl=en"><i class="ai ai-google-scholar-square"></i></a>
+          <a class="social-icon" href="//github.com/smatthewenglish"><i class="fa fa-github"></i></a>
+          <a class="social-icon" href="//open.spotify.com/user/smatthewenglish"><i class="fa fa-spotify"></i></a>
+          <a class="social-icon" href="//www.reddit.com/user/s-matthew-english"><i class="fa fa-reddit"></i></a>
+          <a class="social-icon" href="//instagram.com/smatthewenglish"><i class="fa fa-instagram"></i></a>
+          <a class="social-icon" href="//twitter.com/smatthewenglish"><i class="fa fa-twitter"></i></a>
+          <a class="social-icon" href="//medium.com/@s.matthew.english"><i class="fa fa-medium"></i></a>
+          <a class="social-icon" href="//soundcloud.com/laviusalens"><i class="fa fa-soundcloud"></i></a>  
+          <a class="social-icon" href="//www.youtube.com/channel/UCwvrxkqadxUUAIlsH5zqg3A"><i class="fa fa-youtube"></i></a>
+        </div>
+    </div>    
 </div>
-
-
-      
-
-    </div>
-
-    );
-}
+)}
  
 export default MenuTsc;
-
-// <img class="sme-00" src="./masterpiece.svg"/>
-// <img class="sme-00" src="./masterpiece.gif"/>
-// <img class="sme-00" src="./whoami.gif"/>
-// <img class="sme-00" src="./credentials.gif"/>
