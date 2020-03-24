43bec67ff1ebee00a1d1575233a903dfcd5f677a
smatthewenglish <s.matthew.english@gmail.com>
2020-03-23 22:28:15 -0400

    ~

--- src/WhoAm03.js
+++ src/WhoAm03.js
@@ -4,11 +4,34 @@ import React from 'react';
 const WhoAm03 = () => {
     return (
      
-    <div class="row header">
+    <div class="lolol">
 
-      <div id="zip-00"></div>
-      
-      <img class="sme-00" src="./craftsmanship.svg"/>
+<div id="background-wrap">
+
+    <div class="x1">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x2">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x3">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x4">
+        <div class="cloud"></div>
+    </div>
+
+    <div class="x5">
+        <div class="cloud"></div>
+    </div>
+<img class="craftsmanship" src="./craftsmanship.gif"/>
+
+</div>
+
+
       
 
     </div>
