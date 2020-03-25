9c12df3b2e54cc8d068e119b19724d2d171efe5a
smatthewenglish <s.matthew.english@gmail.com>
2020-03-24 21:46:09 -0400

    zlaps

--- src/MenuEdu.js
+++ src/MenuEdu.js
@@ -2,13 +2,40 @@ import React from 'react';
 import ParticlesBg from "particles-bg";
  
 const MenuEdu = () => {
+
+	let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [5, 40],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      // body: "./img/icon.png", // Whether to render pictures
+      rotate: [0, 20],
+      alpha: [0.6, 0],
+      scale: [1, 0.1],
+      position: "all", // all or center or {x:1,y:1,width:100,height:100}
+      color: ["random", "#ff0000"],
+      cross: "bround", // cross or bround
+      random: 15,  // or null,
+      g: 5,    // gravity
+      f: [2, -1], // force
+      onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+      }
+    };
+
     return (
      
 <div class="box">
 
   <div class="row header">
 
-    <div id="zip-00"><ParticlesBg type="fountain" num={10} bg={true} /></div>
+    <div id="zip-00"><ParticlesBg type="custom" config={config} bg={true} /></div>
     
     <img class="title" src="./credentials.gif" />
  
